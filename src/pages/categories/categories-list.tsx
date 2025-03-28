3180751b8c79691cb6c2c0760dac6c1ca994c915
Anouar-AIT-ELAAOUD <anouar@wiggli.io>
2025-03-28 03:01:10 +0000

    feat: add actions

--- src/pages/categories/categories-list.tsx
+++ src/pages/categories/categories-list.tsx
@@ -1,25 +1,62 @@
+import React, { useState } from "react";
 import { CategoryCard } from "./components/category-card";
 import CategoriesEmptyState from "./components/categories-empty-state";
 import CategoriesLoadingState from "./components/categories-loading-state";
 import CategoriesErrorState from "./components/categories-error-state";
 import { useGetCategories } from "../../hooks/queries/categories/use-get-categories";
+import { Button } from "../../components/ui/button";
+import CategoryModal from "./components/category-modal";
+import { useCreateCategory } from "../../hooks/mutations/categories/use-create-category";
+import { Category } from "../../types";
 
-const CategoriesList = () => {
+const CategoriesList: React.FC = () => {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [categoryName, setCategoryName] = useState<string>("");
+
   const { data: categories, isLoading, error } = useGetCategories();
+  const { mutate: createCategoryMutation, isPending: isCreating } =
+    useCreateCategory();
+
+  const handleSaveCategory = (categoryName: string) => {
+    createCategoryMutation(categoryName);
+    setIsModalOpen(false);
+  };
 
   return (
     <div>
-      <h2 className="text-2xl font-semibold text-center sticky top-0 bg-blue-500 py-4  z-10">
-        Browse Categories
-      </h2>
+      <div className="flex justify-between items-center py-4 z-10 sticky top-0 bg-white">
+        <h2 className="text-2xl text-gray-800 font-semibold text-center">
+          Categories list
+        </h2>
+        <Button
+          onClick={() => {
+            setIsModalOpen(true);
+          }}
+          className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition duration-300"
+        >
+          Create Category
+        </Button>
+      </div>
+
       {isLoading && <CategoriesLoadingState />}
       {error && <CategoriesErrorState />}
       {(!categories || categories.length === 0) && <CategoriesEmptyState />}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-6 py-10">
-        {categories?.map((category) => (
+        {categories?.map((category: Category) => (
           <CategoryCard key={category.id} category={category} />
         ))}
       </div>
+
+      <CategoryModal
+        isOpen={isModalOpen}
+        onClose={() => {
+          setIsModalOpen(false);
+        }}
+        onSave={handleSaveCategory}
+        categoryName={categoryName}
+        setCategoryName={setCategoryName}
+        isLoading={isCreating}
+      />
     </div>
   );
 };
--- src/pages/categories/components/category-card.tsx
+++ src/pages/categories/components/category-card.tsx
@@ -1,33 +1,90 @@
+import { Button } from "../../../components/ui/button";
 import { Card } from "../../../components/ui/card";
 import { Category } from "../../../types";
+import { Trash2, Edit2, Eye } from "lucide-react"; // Import Edit icon
+import { useDeleteCategory } from "../../../hooks/mutations/categories/use-delete-category";
+import { useState } from "react";
+import CategoryModal from "./category-modal";
+import { useUpdateCategory } from "../../../hooks/mutations/categories/use-update-category";
 
-export const CategoryCard = ({ category }: { category: Category }) => {
+interface CategoryCardProps {
+  category: Category;
+}
+
+export const CategoryCard = ({ category }: CategoryCardProps) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [categoryToUpdate, setCategoryToUpdate] = useState<Category | null>(
+    null
+  );
+
+  const { mutate: deleteCategoryMutation, isPending: isDeleting } =
+    useDeleteCategory(category.id);
+  const { mutate: updateCategory, isPending: isUpdating } = useUpdateCategory();
+
   return (
-    <Card
-      key={category.id}
-      className="relative p-6 bg-white rounded-xl shadow-lg hover:shadow-2xl transition-shadow duration-300 ease-in-out group cursor-pointer"
-      onClick={() => {
-        window.location.href = "products";
-        //TODO: set selected category
-      }}
-    >
-      <h2 className="text-2xl font-bold text-gray-800 mb-4 group-hover:text-blue-600 transition-colors duration-200 focus:outline-none">
-        {category.name}
-      </h2>
-      {category.subcategories && category.subcategories.length > 0 ? (
-        <ul className="ml-4 space-y-2">
-          {category.subcategories.map((sub: Category) => (
-            <li
-              key={sub.id}
-              className="flex items-center text-gray-600 group-hover:text-gray-800 transition-colors duration-200"
+    <>
+      <Card className="relative p-6 bg-white rounded-xl shadow-lg hover:shadow-2xl transition-shadow duration-300 ease-in-out group">
+        <h2
+          className="text-2xl font-bold text-gray-800 mb-4 group-hover:text-blue-600 transition-colors duration-200 truncate"
+          title={category.name}
+        >
+          {category.name}
+        </h2>
+
+        <div className="flex justify-between items-center mt-4">
+          <Button
+            className="text-white bg-blue-600 hover:bg-blue-700 transition"
+            onClick={() => {
+              window.location.href = "products";
+            }}
+            title="Preview"
+          >
+            <Eye size={16} />
+          </Button>
+
+          <div className="flex justify-end items-center gap-2">
+            <Button
+              className="text-white bg-green-600 hover:bg-green-700 transition"
+              onClick={() => {
+                setCategoryToUpdate(category);
+                setIsModalOpen(true);
+              }}
+              title="Update"
             >
-              <span className="font-medium">{sub.name}</span>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p className="text-gray-500 mt-2">No subcategories available</p>
+              <Edit2 size={16} />
+            </Button>
+
+            <Button
+              variant="destructive"
+              onClick={() => deleteCategoryMutation()}
+              disabled={isDeleting}
+              className="flex items-center gap-2"
+              title="Delete"
+            >
+              <Trash2 size={16} />
+            </Button>
+          </div>
+        </div>
+      </Card>
+
+      {isModalOpen && categoryToUpdate && (
+        <CategoryModal
+          isOpen={isModalOpen}
+          onClose={() => setIsModalOpen(false)}
+          onSave={(updatedCategoryName: string) => {
+            updateCategory({
+              id: categoryToUpdate.id,
+              name: updatedCategoryName,
+            });
+            setIsModalOpen(false);
+          }}
+          categoryName={categoryToUpdate.name}
+          setCategoryName={(name: string) => {
+            setCategoryToUpdate((prev) => (prev ? { ...prev, name } : null));
+          }}
+          isLoading={isUpdating}
+        />
       )}
-    </Card>
+    </>
   );
 };
