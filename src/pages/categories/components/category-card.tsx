4dafb8fc2670194d0f25ceaf0affaa94a6e61d24
Anouar-AIT-ELAAOUD <anouar@wiggli.io>
2025-03-31 21:31:09 +0000

    feat: display subCategories

--- src/pages/categories/components/category-card.tsx
+++ src/pages/categories/components/category-card.tsx
@@ -21,17 +21,47 @@ export const CategoryCard = ({ category }: CategoryCardProps) => {
     useDeleteCategory(category.id);
   const { mutate: updateCategory, isPending: isUpdating } = useUpdateCategory();
 
+  const renderSubcategories = (subcategories: Category[]) => {
+    return (
+      <ul className="list-disc pl-5">
+        {subcategories.map((subCategory) => (
+          <li key={subCategory.id} className="text-gray-600">
+            {subCategory.name}
+            {subCategory.subcategories &&
+              subCategory.subcategories.length > 0 && (
+                <div className="ml-4">
+                  {renderSubcategories(subCategory.subcategories)}
+                </div>
+              )}
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
   return (
     <>
       <Card className="relative p-6 bg-white rounded-xl shadow-lg hover:shadow-2xl transition-shadow duration-300 ease-in-out group">
-        <h2
-          className="text-2xl font-bold text-gray-800 mb-4 transition-colors duration-200 truncate"
-          title={category.name}
-        >
-          {category.name}
-        </h2>
+        <div className="flex flex-col justify-between h-full">
+          <h2
+            className="text-2xl font-bold text-gray-800 mb-4 transition-colors duration-200 truncate"
+            title={category.name}
+          >
+            {category.name}
+          </h2>
+          {category.subcategories && category.subcategories.length > 0 ? (
+            <div>
+              <h4 className="text-lg font-medium text-gray-700">
+                Subcategories:
+              </h4>
+              {renderSubcategories(category.subcategories)}
+            </div>
+          ) : (
+            <p className="text-red-600">No subcategories available</p>
+          )}
+        </div>
 
-        <div className="flex justify-between items-center mt-4">
+        <div className="flex justify-between items-center mt-4  gap-4">
           <Button
             className="text-black bg-slate-100 hover:bg-slate-200"
             onClick={() => {
