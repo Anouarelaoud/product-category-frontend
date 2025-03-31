41411e013f3f97612096befc3ee4a979bc122bc1
Anouar-AIT-ELAAOUD <anouar@wiggli.io>
2025-03-31 21:32:53 +0000

    feat: products empty state

--- src/pages/products/components/products-empty-state.tsx
+++ src/pages/products/components/products-empty-state.tsx
@@ -2,7 +2,7 @@ const CategoriesEmptyState = () => {
   return (
     <div className="flex justify-center items-center min-h-screen">
       <p className="text-red-600 text-2xl font-semibold">
-        No categories available now. Please try again later.
+        No products available now. Please try again later.
       </p>
     </div>
   );
